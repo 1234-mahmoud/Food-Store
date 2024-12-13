c83bc6d53b41df6ce4fe38e94d3d93ce5b65ff2c
mahmoud elbalhi <mahmoud1234elbalhi@gmail.com>
2024-12-14 01:14:12 +0200

    up

--- food/src/App.jsx
+++ food/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Links from './components/Links';
+import Sale from './components/Sale';
 
 function App() {
   return (
     <div className="App">
     <Header/>
     <Links/>
+    <Sale/>
     </div>
   );
 }
